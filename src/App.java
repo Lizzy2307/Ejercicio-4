e2cf5fcf4b6b52531c5a95dd53195f7cb94b23c3
Lizzy2307 <caceresyani942@gmail.com>
2022-05-30 22:05:27 -0600

    Se agrega el Proyecto

---
+++ src/App.java
@@ -0,0 +1,20 @@
+//Ejercicio 2.18
+//Escriba una aplicación que muestre un cuadro, un óvalo, una flecha
+//y un diamante usando asteriscos (*)
+
+public class Ejercicio2_18
+{
+    public static void main (String[]args)
+    {
+        System.out.print("\n");
+        System.out.println("*********   ***     *       *");
+        System.out.println("*       *  *   *   ***     * *");
+        System.out.println("*       * *     * *****   *   *");
+        System.out.println("*       * *     *   *    *     *");
+        System.out.println("*       * *     *   *   *       *");
+        System.out.println("*       * *     *   *    *     *");
+        System.out.println("*       * *     *   *     *   *");
+        System.out.println("*       *  *   *    *      * *");
+        System.out.println("*********   ***     *       *");
+    }
+}
